9199f03b5e692038852b48b7fed48b018798d192
Patrick <skovskiver@gmail.com>
2021-02-08 11:56:59 +0100

    Er det fredag program, fuldført.

--- src/main/java/com/example/demo/services/CalculateDay.java
+++ src/main/java/com/example/demo/services/CalculateDay.java
@@ -9,12 +9,12 @@ public class CalculateDay {
         Date date = new Date();
         int q = date.getDay();
         int m = date.getMonth();
-        int j = date.getYear()/100;
-        int k = date.getYear()%100;
+        int j = date.getYear()/100;  //Århundredet
+        int k = date.getYear()%100;  //Året i århundredet
 
-        int h = (q + ((13 * (m + 1)) / 5) + k + (k / 4) + (j / 4) - 2 * j) % 7;
+        int h = (q + ((13 * (m + 1)) / 5) + k + (k / 4) + (j / 4) - 2 * j) % 7;  //Midlertidig variabel
 
-        int d = ((h + 5) % 7) + 1;
+        int d = ((h + 5) % 7) + 1;  //Hvilken dag det er i ugen, udregnet som et tal mellem 1-7
 
 
         if (d != 5){
